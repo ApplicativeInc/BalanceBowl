0b5ea535236f2b3e3b4aeb79381a93db05d79cf1
Yang Gao <yg468@nyu.edu>
2014-04-27 20:33:46 -0400

    New game over screen

--- balanceBar.js
+++ balanceBar.js
@@ -18,9 +18,13 @@ var scoreText;
 var star;
 var star_x;
 var star_y;
+var life = 3;
+var lifeText;
+var playButton;
 
 function create() {
     cursors = game.input.keyboard.createCursorKeys();
+    playButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     game.add.sprite(0, 0, 'sky');
 
@@ -31,16 +35,14 @@ function create() {
     pad = platforms.create(200, game.world.height - 64, 'pad');
     pad.body.immovable = true;
 
-    player = game.add.sprite(300, 150, 'dude');
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-    player.body.gravity.y = 600;
-    player.checkWorldBounds = true;
+    player = createPlayer();
 
     stars = game.add.group();
     stars.enableBody = true;
     createNewStar(stars);
 
     scoreText = game.add.text(16, 16, 'Score: 0', {fontSize: '32px', fill: '#000'});
+    lifeText = game.add.text(680, 16, 'Life: 3', {fontSize: '32px', fill: '#000'});
 }
 
 function collectStar(player, star) {
@@ -57,16 +59,47 @@ function createNewStar(stars) {
     star.body.gravity.y = 100;
 }
 
-function update() {
-    game.physics.arcade.collide(player, platforms);
-    // game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+function createPlayer() {
+    var player = game.add.sprite(300, 150, 'dude');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.gravity.y = 600;
+    player.checkWorldBounds = true;
+    player.events.onOutOfBounds.add(resetPlayer, this);
+    return player;
+}
 
-    if (cursors.left.isDown) {
-        player.body.velocity.x -= 30;
-    } else if (cursors.right.isDown) {
-        player.body.velocity.x += 30;
+function resetPlayer(player) {
+    player.reset(300, 150);
+    life -= 1;
+    lifeText.text = 'Life: ' + life;
+    if (life == 1) {
+        player.events.onOutOfBounds.removeAll(this);
+        player.events.onOutOfBounds.add(gameOver, this);
     }
-    star.body.x = star_x + (Math.random() - 0.5) * 2;
-    star.body.y = star_y + (Math.random() - 0.5) * 2;
+}
+
+function gameOver() {
+    game.add.text(300, 200, 'GAME OVER', {fontSize: '64px', fill: '#000'});
+    game.add.text(220, 233, 'Press Space to Restart', {fontSize: '24px', fill: '#000'});
+}
+
+function update() {
+    if (life == 0) {
+        if (playButton.isDown) {
+            life = 3;
+            score = 0;
+            resetPlayer();
+        }
+    } else {
+        game.physics.arcade.collide(player, platforms);
+        game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+        if (cursors.left.isDown) {
+            player.body.velocity.x -= 30;
+        } else if (cursors.right.isDown) {
+            player.body.velocity.x += 30;
+        }
+        star.body.x = star_x + (Math.random() - 0.5) * 2;
+        star.body.y = star_y + (Math.random() - 0.5) * 2;
+    }
 }
